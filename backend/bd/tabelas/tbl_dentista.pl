ed0a6fda95a10aceffd8c49c5753b96a3c8a0632
Matheus Antonio <79953129+Maantz@users.noreply.github.com>
2021-06-14 20:04:39 -0300

    Delete tbl_dentista.pl

--- backend/bd/tabelas/tbl_dentista.pl
+++
@@ -1,2 +0,0 @@
-created(1623709830.208926).
-assert(dentista(4,"CRO-MG 2323","1111",'$pbkdf2-sha512$t=131072$IeKay3j6BhBPTI023aTH6Q$/4J6bwBudhnVBF7s6KBnXv3J3DyiHPzGawj0e6Pe/zirainitqzjOqy5zUBpeArtSXxHZNssOaM0dZ9yx8AAjA')).
