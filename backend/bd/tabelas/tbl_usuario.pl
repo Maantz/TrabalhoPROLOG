2ddc33fa538ab0e4610e05e30c5936e2dc0a246d
Matheus Antonio <79953129+Maantz@users.noreply.github.com>
2021-06-14 20:05:38 -0300

    Delete tbl_usuario.pl

--- backend/bd/tabelas/tbl_usuario.pl
+++
@@ -1,5 +0,0 @@
-created(1623709830.209922).
-assert(usuario(1,"Gustavo","gustavoguimaraesreis@hotmail.com",'$pbkdf2-sha512$t=131072$4tIRWRm+bS4Sl53Z9iplAA$IXlY5dqqUON1QVsoqy0u+sZMkdEA33g7D1zdXAQLScewKCnKUtNf5krV6qtl0Lj3Ungtksu/vt88Nf9jz8zeFQ')).
-assert(usuario(2,"Filipe","filipegui@hotmail.com",'$pbkdf2-sha512$t=131072$h1Ze+pYY3kkFnxNau74UBQ$CHJ0BaArU2DwjpHHVPufnzfwctpWKQtnPuVDVmWkZ0fkbAMXF4LNBDJl5s9kb0cx41OkS8q72lKEiUKsl2y19w')).
-assert(usuario(5,"mayra","mayra123@hotmail.com",'$pbkdf2-sha512$t=131072$I6H/vxjktg25jloGr+0dwA$wb61Op3FPGJLuuNbr95YKIPVMx0R+D5K+sTWa9mWADvGXttvE2ZljjxzhjRpV/tzgWAFpekqo2/Jktejzna9ag')).
-assert(usuario(6,"gustavo g reis","gustavinhodograu@hotmail.com",'$pbkdf2-sha512$t=131072$JmtlCGbFlUzrl0/3i+0PIg$tXC7RHgAgqbXscr0op5IKc0R+bBLaCW0uh14nB3Q9j8vx6D4dBqH4U7IFXSOzH2L+7DBX8CncWs7JKJo37cSOw')).
