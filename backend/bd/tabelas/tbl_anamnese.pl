a328565c6492884247824725cebb7069f67550f8
gustavoguimaraesreis <gustavoguimaraesreis@ufu.br>
2021-06-16 01:43:30 -0300

    save

--- backend/bd/tabelas/tbl_anamnese.pl
+++ backend/bd/tabelas/tbl_anamnese.pl
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-created(1623818085.648357).
-=======
 created(1623817461.249511).
->>>>>>> ba2ea90487f2d1f7d4ef1959aa7189c1c256ef61
 assert(anamnese(1,"Dipirona","A+","N/A","Plazil","sim","nao")).
--- backend/bd/tabelas/tbl_dentista.pl
+++ backend/bd/tabelas/tbl_dentista.pl
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-created(1623818085.650351).
-=======
 created(1623817461.249511).
->>>>>>> ba2ea90487f2d1f7d4ef1959aa7189c1c256ef61
 assert(dentista(9,"1515215131513")).
 assert(dentista(10,"000")).
